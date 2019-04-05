42747092a74122e6e11bda13b69a88b2fa8b4050
mjaracz <michal19996@gmail.com>
2019-04-05 13:46:32 +0200

    Updates

---
+++ precache-manifest.aac061ff3304ee164bc859970ae441bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9c9df41d6f58ab6d8b2",
+    "url": "/turing-rec/static/js/runtime~main.f93e0b23.js"
+  },
+  {
+    "revision": "0ba4e734d4935a378788",
+    "url": "/turing-rec/static/js/main.711c7deb.chunk.js"
+  },
+  {
+    "revision": "bea0cf2eccfeae5ab1b6",
+    "url": "/turing-rec/static/js/2.b93c0ff2.chunk.js"
+  },
+  {
+    "revision": "0ba4e734d4935a378788",
+    "url": "/turing-rec/static/css/main.726e7883.chunk.css"
+  },
+  {
+    "revision": "614b92c567721a1a3db5edd5f5634677",
+    "url": "/turing-rec/index.html"
+  }
+];
